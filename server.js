4e78ac07959861534ee467fabae98d3dbb5c3e40
mhmmdarifrivai12 <mhmmdarifrivai12@gmail.com>
2025-02-21 23:56:39 +0700

    Pesan commit yang menjelaskan perubahan

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/teacher', teacherRoutes);
 app.use('/api/public', publicRoutes);
 
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from Vercel API!" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
